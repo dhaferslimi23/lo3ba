2ab5f1f2e5d7c0c49ececcb1f38e7f6523875d96
Ben Miled Mohamed <47534906+garille@users.noreply.github.com>
2019-04-08 09:26:21 +0100

    Create Enigme.h

---
+++ Enigme.h
@@ -0,0 +1,4 @@
+int affichEnigme(x , SDL_Surface*screen , *image , SDL_Rect positionecran);
+int ReponseEnigme(SDL_Event test , int x);
+int Win(SDL_Surface *screen , SDL_Surface *image, SDL_Rect positionecran);
+int Lose(SDL_Surface *screen , SDL_Surface *image, SDL_Rect positionecran);
